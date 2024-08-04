75ec542365387bf787e8b7cae38c8be981b448fd
nikatgithub <kasabnikhil@gmail.com>
2024-08-04 19:26:03 +0530

    Initialised the project backend

---
+++ src/main/java/com/teamCollaboration/RemoteTeamCollaborationApplication.java
@@ -0,0 +1,13 @@
+package com.teamCollaboration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteTeamCollaborationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemoteTeamCollaborationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamCollaboration/RemoteTeamCollaborationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamCollaboration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemoteTeamCollaborationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
